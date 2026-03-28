5e97b20ee3a9f27c88166802994a80308b6e09cb
Jeremy Billote <jbillote@gmail.com>
2026-03-28 16:21:31 -0700

    refactor: move streamers routes into subdirectory

---
+++ server/api/v1/streamers/index.ts
@@ -0,0 +1,30 @@
+import { Elysia } from 'elysia'
+import channels from '../../../data/channels.json'
+
+export default () =>
+  new Elysia().get('/v1/streamers', () => {
+    const orgs = []
+    channels.forEach((org) => {
+      const branches = []
+      org.branches.forEach((branch) => {
+        const members = []
+        branch.members.forEach((member) => {
+          members.push({
+            name: member.name,
+          })
+        })
+
+        branches.push({
+          name: branch.name,
+          members: members,
+        })
+      })
+
+      orgs.push({
+        name: org.name,
+        branches: branches,
+      })
+    })
+
+    return orgs
+  })
--- server/index.ts
+++ server/index.ts
@@ -1,30 +1,4 @@
 import { Elysia } from 'elysia'
-import channels from './data/channels.json'
+import streamersRoute from './api/v1/streamers'
 
-export default () =>
-  new Elysia().get('/v1/streamers', () => {
-    const orgs = []
-    channels.forEach((org) => {
-      const branches = []
-      org.branches.forEach((branch) => {
-        const members = []
-        branch.members.forEach((member) => {
-          members.push({
-            name: member.name
-          })
-        })
-
-        branches.push({
-          name: branch.name,
-          members: members
-        })
-      })
-
-      orgs.push({
-        name: org.name,
-        branches: branches
-      })
-    })
-
-    return orgs
-  })
+export default () => new Elysia().use(streamersRoute)
