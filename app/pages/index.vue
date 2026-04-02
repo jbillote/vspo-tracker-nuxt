2bc9417613da05f53a646b94c6470c9f8a843187
jbillote <jbillote@gmail.com>
2026-04-02 09:31:52 -0700

    feat: front page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,40 @@
+<script setup lang="ts">
+import VideoCard from '@/components/VideoCard.vue'
+import { DateTime } from 'luxon'
+
+const { $api } = useNuxtApp()
+const { data } = await useAsyncData(async () => {
+  const { data: resp, error } = await $api.v1.videos.live.get()
+
+  if (error) {
+    throw new Error('Unable to load data')
+  }
+
+  return resp
+})
+
+const upcoming = computed(() => {
+  return (
+    data.value.upcoming?.filter((video) => {
+      return video.scheduledStart && DateTime.fromISO(video.scheduledStart).diffNow('days').days < 2
+    }) ?? []
+  )
+})
+</script>
+
 <template>
-  <div>Test</div>
+  <div>
+    <h1 class="mt-2 text-center text-4xl font-bold">Live</h1>
+    <div class="flex flex-wrap justify-center p-2">
+      <div class="flex flex-wrap justify-center p-2">
+        <VideoCard v-for="video in data?.live" :key="video.url" v-bind="video" />
+      </div>
+    </div>
+    <h1 class="mt-2 text-center text-4xl font-bold">Upcoming</h1>
+    <div class="flex flex-wrap justify-center p-2">
+      <div class="flex flex-wrap justify-center p-2">
+        <VideoCard v-for="video in upcoming" :key="video.url" v-bind="video" />
+      </div>
+    </div>
+  </div>
 </template>
