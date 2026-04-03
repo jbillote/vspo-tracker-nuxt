c4e31f86b60e6540bac681c53778d6b5d8088901
jbillote <jbillote@gmail.com>
2026-04-02 22:16:28 -0700

    chore: update header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'VSPO! Tracker',
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
