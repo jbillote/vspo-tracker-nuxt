b8e8d3cffba75873e6e63ab054782abd8d0b1a1e
jbillote <jbillote@gmail.com>
2026-04-06 13:31:27 -0700

    feat: filter button and dialog

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,44 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
 import { Separator } from '@/components/ui/separator'
+import { ListFilter } from 'lucide-vue-next'
 </script>
 
 <template>
   <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
     <Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
-    <span>VSPorte! Tracker</span>
+    <span class="flex-auto">VSPO! Tracker</span>
+    <Dialog>
+      <DialogTrigger as-child>
+        <Button class="hover:bg-accent bg-transparent">
+          <ListFilter class="text-white" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent class="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Filter</DialogTitle>
+          <DialogDescription>Filter</DialogDescription>
+        </DialogHeader>
+        <div>
+          <h1 class="text-center text-3xl font-bold">Filter</h1>
+        </div>
+        <DialogFooter>
+          <DialogClose as-child>
+            <Button variant="outline">Close</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   </header>
   <div class="flex flex-1 flex-col gap-4 p-4">
     <NuxtPage />
