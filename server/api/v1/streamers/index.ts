dfce07a51a99fd3f266f4506468c68bab797a4d5
jbillote <jbillote@gmail.com>
2026-04-09 01:08:08 -0700

    feat: /api/v1/streamers/orgs

--- server/api/v1/streamers/index.ts
+++ server/api/v1/streamers/index.ts
@@ -2,30 +2,39 @@ import { Elysia } from 'elysia'
 import channels from '../../../data/channels.json'
 
 export default function createStreamersRoutes() {
-  return new Elysia().get('/v1/streamers', () => {
-    const orgs = []
-    channels.forEach((org) => {
-      const branches = []
-      org.branches.forEach((branch) => {
-        const members = []
-        branch.members.forEach((member) => {
-          members.push({
-            name: member.name,
+  return new Elysia({ prefix: '/v1/streamers' })
+    .get('/', () => {
+      const orgs = []
+      channels.forEach((org) => {
+        const branches = []
+        org.branches.forEach((branch) => {
+          const members = []
+          branch.members.forEach((member) => {
+            members.push({
+              name: member.name,
+            })
+          })
+
+          branches.push({
+            name: branch.name,
+            members: members,
           })
         })
 
-        branches.push({
-          name: branch.name,
-          members: members,
+        orgs.push({
+          name: org.name,
+          branches: branches,
         })
       })
 
-      orgs.push({
-        name: org.name,
-        branches: branches,
-      })
+      return orgs
     })
+    .get('/orgs', () => {
+      const orgs = []
+      channels.forEach((org) => {
+        orgs.push(org.name)
+      })
 
-    return orgs
-  })
+      return orgs
+    })
 }
