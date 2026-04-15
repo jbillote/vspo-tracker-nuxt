adf5d8d69c8b01571f8e770b19a3aeda46e41a1e
Jeremy Billote <jbillote@gmail.com>
2026-04-14 20:34:34 -0700

    fix: show upcoming stream time in local time zone by rendering on client

--- app/components/VideoCard.vue
+++ app/components/VideoCard.vue
@@ -54,18 +54,20 @@ defineProps<{
         </a>
       </div>
       <div class="max-w-64 text-sm">
-        <span
-          class="relative z-10 cursor-pointer select-none"
-          :title="
-            status !== 'live'
-              ? DateTime.fromISO(scheduledStart)?.toLocaleString(DateTime.DATETIME_FULL)
-              : ''
-          "
-        >
-          {{
-            (status === 'live' ? 'Started ' : '') + DateTime.fromISO(scheduledStart)?.toRelative()
-          }}
-        </span>
+        <ClientOnly>
+          <span
+            class="relative z-10 cursor-pointer select-none"
+            :title="
+              status !== 'live'
+                ? DateTime.fromISO(scheduledStart)?.toLocal().toLocaleString(DateTime.DATETIME_FULL)
+                : ''
+            "
+          >
+            {{
+              (status === 'live' ? 'Started ' : '') + DateTime.fromISO(scheduledStart)?.toRelative()
+            }}
+          </span>
+        </ClientOnly>
       </div>
     </div>
     <a :href="url" class="absolute inset-0" />
