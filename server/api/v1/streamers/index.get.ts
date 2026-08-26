f6f0b74bdb737617d6ae9dd900fd91aa6eee65eb
jbillote <jbillote@gmail.com>
2026-08-25 21:14:20 -0700

    feat: categorize streamer api

--- server/api/v1/streamers/index.get.ts
+++ server/api/v1/streamers/index.get.ts
@@ -1,12 +1,15 @@
 import channels from '../../../data/channels.json'
 
 export default defineEventHandler(async () => {
-  const streamers: string[] = []
+  const streamers = {}
   for (const org of channels) {
     for (const branch of org.branches) {
+      const key = org.branches.length > 1 ? `${org.name} ${branch.name}` : org.name
+      const orgStreamers = []
       for (const member of branch.members) {
-        streamers.push(member.name)
+        orgStreamers.push(member.name)
       }
+      streamers[key] = orgStreamers
     }
   }
 
